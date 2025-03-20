a1144695bdcbbf0979b7a8fd73a94791472b5d8d
Bruno Donadei <bruno.donadei@hiplatform.com>
2025-03-20 00:22:02 -0300

    feat: add cards with name, address, city, state, phone and country of the breweries

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,28 @@ import { useGetBrewerie } from "./hooks/useGetBrewerie";
 function App() {
   const { data, loading } = useGetBrewerie();
   console.log(data, loading);
-  return <></>;
+  return (
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(4, 1fr)",
+        gap: "15px",
+      }}
+    >
+      {data?.map((d) => {
+        return (
+          <div style={{ background: "#a0a0a0", maxWidth: "280px" }} key={d.id}>
+            <h2>{d.name}</h2>
+            <span>{d.address_1}</span>
+            <span>
+              {d.city} {d.state} - {d.phone}
+            </span>
+            <span>{d.country}</span>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
